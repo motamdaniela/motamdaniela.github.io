c2c685db85a813af56ba3a69c349e7c5e47be37b
Daniela <92992969+motadaniela@users.noreply.github.com>
2023-08-24 23:39:57 +0100

    :)

--- assets/js/mainFile.js
+++ assets/js/mainFile.js
@@ -18,27 +18,32 @@ let blurcontent = document.querySelectorAll('.blurover');
 // let opts = document.querySelector('.CoverOpt');
 // const $hover = document.querySelectorAll('a');
 
-img.style.right = filter.style.right = (2*window.innerWidth/6)+"px";
-img.style.bottom = filter.style.bottom = (window.innerHeight/8)+"px";
 // filter.style.right = img.style.right
 // filter.style.bottom = img.style.bottom
 
+if(window.innerWidth > 800){
+    img.style.right = filter.style.right = (2*window.innerWidth/6)+"px";
+    img.style.bottom = filter.style.bottom = (window.innerHeight/8)+"px";
 
-document.body.addEventListener('mousemove', function(e){
-    //for the cursor
-    //* crclCursor.style.left = e.clientX -32 +'px';
-    //* crclCursor.style.top = e.clientY-32 +'px';
+    document.body.addEventListener('mousemove', function(e){
+        //for the cursor
+        //* crclCursor.style.left = e.clientX -32 +'px';
+        //* crclCursor.style.top = e.clientY-32 +'px';
+    
+        // for the cover image
+        img.style.right = filter.style.right = (e.clientX - (window.innerWidth/2 - img.width/2))+"px";
+        img.style.bottom = filter.style.bottom = (e.clientY - (window.innerHeight/2 - img.height/2))+"px";
+    
+        content.style.left = (e.clientX - (window.innerWidth/2 ))/40+"px";
+        content.style.top = (e.clientY - (window.innerHeight/2 ))/40+"px";
+    
+    });
+}else{
+    img.style.right = filter.style.right = (1.5*window.innerWidth/6)+"px";
+    img.style.bottom = filter.style.bottom = (window.innerHeight/2)+"px";
+    img.style.width = filter.style.width = '60vw';
+}
 
-    // for the cover image
-    img.style.right = filter.style.right = (e.clientX - (window.innerWidth/2 - img.width/2))+"px";
-    img.style.bottom = filter.style.bottom = (e.clientY - (window.innerHeight/2 - img.height/2))+"px";
-    // filter.style.right = img.style.right
-    // filter.style.bottom = img.style.bottom
-
-    content.style.left = (e.clientX - (window.innerWidth/2 ))/40+"px";
-    content.style.top = (e.clientY - (window.innerHeight/2 ))/40+"px";
-
-});
 
 //for the cover opts
 opts.forEach((opt) => {
@@ -50,14 +55,24 @@ opts.forEach((opt) => {
             if(option.id != opt.id){
                 document.querySelector(`#${option.id}`).style.filter = 'blur(10px)';
             }
-            // else if(option.id == opt.id){
-                // document.querySelector(`.${option.id}`).style = 
-                // `background-image: url('../img/circle.svg');
-                // background-repeat: none;
-                // background-size: 120%;`;
-
-                // document.querySelector(`.${opt.id}`).style.background = '';
-                // document.querySelector(`.${opt.id}`).style.backgroundSize = '90px';
+            else{
+                switch(option.id){
+                    case 'optfront':
+                        document.querySelector('.coverImg').src = '/assets/img/portfolio/tw.png';
+                        break;
+                    case 'optweb':
+                        document.querySelector('.coverImg').src = '/assets/img/portfolio/evergreen.png';
+                        break;
+                    case 'optdig':
+                        document.querySelector('.coverImg').src = '/assets/img/portfolio/video.png';
+                        break;
+                    case 'optint':
+                        document.querySelector('.coverImg').src = '/assets/img/portfolio/eco-meow2.png';
+                        break;
+                }
+            }
+            // if(option.id == optfront){
+            //     document.querySelector('.coverImg').src = '/assets/img/portfolio/tw.png';
             // }
         })
         // document.querySelector(`#${opt.id}`).style.filter = 'blur(0px)';
@@ -76,15 +91,16 @@ opts.forEach((opt) => {
         })
         img.style.filter = 'contrast(80%) saturate(40%) grayscale(10%) brightness(1.1) blur(10px)';
         filter.style.filter ='blur(10px)';
+        // document.querySelector('.coverImg').src = '/assets/img/portfolio/eco-meow2.png';
     });
 });
 
-document.body.addEventListener('mouseleave', function(){
-    //* crclCursor.style.scale = 0;
-})
-document.body.addEventListener('mouseenter', function(){
-    //* crclCursor.style.scale = 1;
-})
+// document.body.addEventListener('mouseleave', function(){
+//     //* crclCursor.style.scale = 0;
+// })
+// document.body.addEventListener('mouseenter', function(){
+//     //* crclCursor.style.scale = 1;
+// })
 
 
 
@@ -95,9 +111,9 @@ let projects = [
         category: 'Web app',
         description: 'a web app for tutorials on rules on the road',
         photo: '/assets/img/portfolio/eco-meow2.png',
+        video: '',
         codeLink: '',
         designLink: '',
-        thumbnail: '',
-        video: '',
+        // videoLink: '',
     }
 ]
